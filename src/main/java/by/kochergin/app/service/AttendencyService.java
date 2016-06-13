246d1b85693e7012823d0d68c5b6713bcde5f80c
Aliaksandr Klimchuk <Aliaksandr_Klimchuk@epam.com>
2016-06-14 00:59:26 +0300

    service annotation

--- src/main/java/by/kochergin/app/service/AttendencyService.java
+++ src/main/java/by/kochergin/app/service/AttendencyService.java
@@ -1,8 +1,10 @@
 package by.kochergin.app.service;
 
+import org.springframework.stereotype.Service;
+
 import by.kochergin.app.dao.IAttendencyDao;
 import by.kochergin.app.domain.Attendency;
-
+@Service
 public class AttendencyService extends GenericService<Attendency, Integer, IAttendencyDao> {
 	@Override
 	public Attendency create(Attendency entity) {
